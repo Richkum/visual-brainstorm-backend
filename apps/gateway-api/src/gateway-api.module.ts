476187894e6d34f27b0d43c4e174840993e05915
Ayuk <giress865@gmail.com>
2025-09-04 23:57:18 +0100

    created socket connection backend

--- apps/gateway-api/src/gateway-api.module.ts
+++ apps/gateway-api/src/gateway-api.module.ts
@@ -1,11 +1,15 @@
 import { Module } from '@nestjs/common';
 import { GatewayApiController } from './gateway-api.controller';
 import { GatewayApiService } from './gateway-api.service';
+<<<<<<< HEAD
 import { HttpModule } from '@nestjs/axios';
+=======
+import { GatewayGateway } from './gateway.gateway';
+>>>>>>> 7822982 (created socket connection backend)
 
 @Module({
   imports: [HttpModule],
   controllers: [GatewayApiController],
-  providers: [GatewayApiService],
+  providers: [GatewayApiService, GatewayGateway],
 })
 export class GatewayApiModule {}
